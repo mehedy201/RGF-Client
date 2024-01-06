5107ba15d12ca5d05048cbabd8f194adebd70bcb
mehedy201 <mehedy643@gmail.com>
2024-01-07 02:20:44 +0600

    now working fine  the calculetor page calculetor almost done front end

--- src/Pages/Calculator/Calculator.jsx
+++ src/Pages/Calculator/Calculator.jsx
@@ -1,8 +1,54 @@
 // import React from 'react';
 
 import { Input, Select } from "antd";
+import { useState } from "react";
 
 const Calculator = () => {
+
+
+    const [selectData, setSelectData ] = useState('Rectangle');
+    const [length, setLength] = useState();
+    const [width, setWidth] = useState();
+    const [deeps, setDeeps] = useState();
+    const [diameter, setDiameter] = useState()
+
+    const [cubicYards, setCubicYards] = useState();
+    const [totalTon, setTotalTon] = useState()
+
+    const rectangleCalculetor = () => {
+        const intLength = parseInt(length)
+        const intWidth = parseInt(width)
+        const intDeeps = parseInt(deeps)
+        const getCubicYards = intLength * intWidth * (intDeeps / 12) / 27;
+        const getTotalTon = getCubicYards * 1.4;
+        setCubicYards(getCubicYards.toFixed(2))
+        setTotalTon(getTotalTon.toFixed(2))
+    }
+
+    const triangleCalculetor = () => {
+        const intLength = parseInt(length)
+        const intWidth = parseInt(width)
+        const intDeeps = parseInt(deeps)
+        const getCubicYards = (1/2) * intLength * intWidth * (intDeeps / 12) / 27;
+        const getTotalTon = getCubicYards * 1.4;
+        setCubicYards(getCubicYards.toFixed(2))
+        setTotalTon(getTotalTon.toFixed(2))
+    }
+
+    const circleCalculetor = () => {
+        const intDiameter = parseInt(diameter)
+        const intDeeps = parseInt(deeps)
+        const squareDiameter = (intDiameter/2) * (intDiameter/2) * 3.14;
+        const getCubicYards = squareDiameter * (intDeeps / 12) / 27;
+        const getTotalTon = getCubicYards * 1.4;
+        setCubicYards(getCubicYards.toFixed(2))
+        setTotalTon(getTotalTon.toFixed(2))
+    }
+
+
+    
+
+
     return (
         <section className="py-5">
             <div className="container">
@@ -21,6 +67,7 @@ const Calculator = () => {
                                 style={{
                                     width: '100%',
                                 }}
+                                onChange={e => setSelectData(e)}
                                 options={[
                                     { value: 'Rectangle', label: 'Rectangle',},
                                     { value: 'Triangle', label: 'Triangle',},
@@ -28,30 +75,54 @@ const Calculator = () => {
                                 ]}
                                 />
                             </div>
-                            {/* Length Feet ____________________________ */}
-                            <div className="col-6 border p-2">
-                                <div className="h-100 d-flex align-items-center"><h6 className="m-0">Length (feet):</h6></div>
-                            </div>
-                            <div className="col-6 border px-2 py-1">
-                                <Input size="large"/>
-                            </div>
-                            {/* Width (feet) ____________________________ */}
-                            <div className="col-6 border p-2">
-                                <div className="h-100 d-flex align-items-center"><h6 className="m-0">Width (feet):</h6></div>
-                            </div>
-                            <div className="col-6 border px-2 py-1">
-                                <Input size="large"/>
-                            </div>
+                            {
+                                selectData !== 'Circle' && <>
+                                {/* Length Feet ____________________________ */}
+                                    <div className="col-6 border p-2">
+                                        <div className="h-100 d-flex align-items-center"><h6 className="m-0">Length (feet):</h6></div>
+                                    </div>
+                                    <div className="col-6 border px-2 py-1">
+                                        <Input onChange={e => setLength(e.target.value)} size="large"/>
+                                    </div>
+                                    {/* Width (feet) ____________________________ */}
+                                    <div className="col-6 border p-2">
+                                        <div className="h-100 d-flex align-items-center"><h6 className="m-0">Width (feet):</h6></div>
+                                    </div>
+                                    <div className="col-6 border px-2 py-1">
+                                        <Input onChange={e => setWidth(e.target.value)} size="large"/>
+                                    </div> 
+                                </>
+                            }
+                            {/* For Circle  */}
+                            {
+                                selectData == 'Circle' && <>
+                                    {/* Length Feet ____________________________ */}
+                                    <div className="col-6 border p-2">
+                                        <div className="h-100 d-flex align-items-center"><h6 className="m-0">Diameter (feet):</h6></div>
+                                    </div>
+                                    <div className="col-6 border px-2 py-1">
+                                        <Input onChange={e => setDiameter(e.target.value)} size="large"/>
+                                    </div>
+                                </>
+                            }
                             {/* Depth (inches): ____________________________ */}
                             <div className="col-6 border p-2">
                                 <div className="h-100 d-flex align-items-center"><h6 className="m-0">Depth (inches):</h6></div>
                             </div>
                             <div className="col-6 border px-2 py-1">
-                                <Input size="large"/>
+                                <Input onChange={e => setDeeps(e.target.value)} size="large"/>
                             </div>
                             <div className="col-12 p-2">
                                 {/* Calculate Button ________________________ */}
-                                <button className="btn bg-dark d-block text-white w-100 fw-bold">Calculate</button>
+                                {
+                                    selectData == 'Rectangle' && <button onClick={() => rectangleCalculetor()} className="btn bg-dark d-block text-white w-100 fw-bold">Calculate</button>
+                                }
+                                {
+                                    selectData == 'Triangle' && <button onClick={() => triangleCalculetor()} className="btn bg-dark d-block text-white w-100 fw-bold">Calculate</button>
+                                }
+                                {
+                                    selectData == 'Circle' && <button onClick={() => circleCalculetor()} className="btn bg-dark d-block text-white w-100 fw-bold">Calculate</button>
+                                }
                             </div>
                             <div className="col-12 p-2">
                                 {/* Clear Button ________________________ */}
@@ -62,14 +133,14 @@ const Calculator = () => {
                                 <div className="h-100 d-flex align-items-center"><h6 className="m-0">Cubic Yards:</h6></div>
                             </div>
                             <div className="col-6 border px-2 py-1">
-                                <Input size="large"/>
+                                <Input value={cubicYards} size="large" readOnly/>
                             </div>
                             {/* Total Tons*: ____________________________ */}
                             <div className="col-6 border p-2">
                                 <div className="h-100 d-flex align-items-center"><h6 className="m-0">Total Tons*:</h6></div>
                             </div>
                             <div className="col-6 border px-2 py-1">
-                                <Input size="large"/>
+                                <Input value={totalTon} size="large" readOnly/>
                             </div>
                         </div>
                         
@@ -77,13 +148,19 @@ const Calculator = () => {
                     {/* Right Side Div ____________________________________________________ */}
                     <div className="col-md-7 p-4">
                         <div>
-                            <p className="fw-bold text-dark">How to figure rectangle manually:</p>
-                            <ol>
-                                <li className="text-secondary small">Length x Width = Square Feet</li>
-                                <li className="text-secondary small">Square Feet ÷ “X” = Cubic Yards (to find “X,” match depth with number in table below)</li>
-                                <li className="text-secondary small">To convert Cubic Yards to Tons*: Cubic Yards x 1.4 = Tons</li>
-                                <li className="text-secondary small">To convert Tons to Cubic Yards*: Tons ÷ 1.4 = Cubic Yards</li>
-                            </ol>
+                            {
+                                selectData == 'Rectangle' && <div>
+                                    <p className="fw-bold text-dark">How to figure rectangle manually:</p>
+                                    <ol>
+                                        <li className="text-secondary small">Length x Width = Square Feet</li>
+                                        <li className="text-secondary small">Square Feet ÷ “X” = Cubic Yards (to find “X,” match depth with number in table below)</li>
+                                        <li className="text-secondary small">To convert Cubic Yards to Tons*: Cubic Yards x 1.4 = Tons</li>
+                                        <li className="text-secondary small">To convert Tons to Cubic Yards*: Tons ÷ 1.4 = Cubic Yards</li>
+                                    </ol>
+                                </div>
+                                
+                                
+                            }
                             <div className="border">
                                 <p style={{fontSize: '14px'}} className="fw-bold p-2 m-0">1.4 is a rule of thumb that will work for most sand and gravel products (1 yard = 2,800#). Some products such as soils and mixes may vary due to their different bulk densities (weight per cu. yd).</p>
                             </div>
